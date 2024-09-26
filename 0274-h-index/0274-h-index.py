f9a70c3599640556a26ae534b3b2d4b5da73a18f
Sara Oraib <saraoraib6@gmail.com>
2024-09-26 21:46:32 +0300

    Time: 22 ms (52.78%), Space: 11.9 MB (72.74%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hIndex(self, citations):
+        citations.sort(reverse=True)  # Sort citations in descending order
+        h_index = 0
+        
+        for i, citation in enumerate(citations):
+            if citation >= i + 1:
+                h_index = i + 1
+            else:
+                break
+        
+        return h_index
